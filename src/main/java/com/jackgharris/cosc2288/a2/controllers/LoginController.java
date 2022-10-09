1bd46d50a4e2801307d037c59f56edcbd27d9930
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-09 21:12:23 +1100

    - Added variable mapping for the text-feilds for email and password input. - Added logic to check for a valid email (Using Dummy check untill DB connection) and redirection to the dashboard stage and scene if successful.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/LoginController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/LoginController.java
@@ -2,14 +2,29 @@ package com.jackgharris.cosc2288.a2.controllers;
 
 import com.jackgharris.cosc2288.a2.core.MyHealth;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
+import com.jackgharris.cosc2288.a2.utility.Resource;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 
+
 public class LoginController {
+
+    @FXML
+    private TextField emailInputField;
+
+    @FXML
+    private TextField passwordInputField;
+
+    @FXML
+    private Label loginErrorLabel;
 
     public LoginController(){
         System.out.println("Registration Controller Created!");
@@ -19,12 +34,34 @@ public class LoginController {
     public void registerButton(ActionEvent event) throws IOException {
         System.out.println("Back Button Pressed!");
         Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(FXMLUtility.loadScene(FXMLUtility.registrationFXML,stage, MyHealth.css));
+        stage.setScene(FXMLUtility.loadScene(FXMLUtility.registrationFXML,stage, MyHealth.launcherCSS));
         stage.show();
     }
 
-    public void loginButton(ActionEvent event){
+    public void loginButton(ActionEvent event) throws IOException {
         System.out.println("Login Button Pressed!");
+        System.out.println("Email = "+this.emailInputField.getText());
+        System.out.println("Password = "+this.passwordInputField.getText());
+
+        if(!this.emailInputField.getText().equals("st_tuff@me.com")){
+
+            this.loginErrorLabel.getStyleClass().remove("notification-hidden");
+            this.emailInputField.getStyleClass().add("text-field-error");
+            this.passwordInputField.getStyleClass().add("text-field-error");
+
+        }else{
+            Stage dashboardStage = new Stage(StageStyle.DECORATED);
+            Stage launcherStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+
+
+            dashboardStage.setScene(FXMLUtility.loadScene(FXMLUtility.dashboardFXML,launcherStage, MyHealth.appCSS));
+            dashboardStage.setResizable(true);
+            dashboardStage.setMaximized(true);
+            dashboardStage.setTitle(MyHealth.title);
+            dashboardStage.getIcons().add(Resource.favicon());
+            dashboardStage.show();
+            launcherStage.hide();
+        }
     }
 
     public void closeButton(ActionEvent event){
