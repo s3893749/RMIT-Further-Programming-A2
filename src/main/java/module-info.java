994804b9edf9b2088db5eddb36358694e196625b
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-09 21:13:02 +1100

    - Added org.xerial.sqlitejdbc as a required dependency.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module com.jackgharris.cosc2288.a2 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.xerial.sqlitejdbc;
 
     exports com.jackgharris.cosc2288.a2.core;
     exports com.jackgharris.cosc2288.a2.controllers;
+
     opens com.jackgharris.cosc2288.a2.core to javafx.fxml;
     opens com.jackgharris.cosc2288.a2.controllers to javafx.fxml;
 }
