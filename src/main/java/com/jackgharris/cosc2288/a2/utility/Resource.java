4b59bb95adb0e44c4b8aae2248fb56e516ab3eb8
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-09 21:14:40 +1100

    - Created Resource object that returns a static reference to the logo.

---
+++ src/main/java/com/jackgharris/cosc2288/a2/utility/Resource.java
@@ -0,0 +1,15 @@
+package com.jackgharris.cosc2288.a2.utility;
+
+import com.jackgharris.cosc2288.a2.core.MyHealth;
+import javafx.scene.image.Image;
+
+import java.util.Objects;
+
+public class Resource {
+
+    public static Image favicon(){
+
+        return new Image(Objects.requireNonNull(MyHealth.class.getResourceAsStream("favicon.png")));
+    };
+
+}
