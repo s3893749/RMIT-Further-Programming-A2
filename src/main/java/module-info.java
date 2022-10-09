ede168aff57fae9c8b2873747c625d021b150cff
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-09 14:40:23 +1100

    - Upload of required maven files.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.jackgharris.cosc2288.a2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    exports com.jackgharris.cosc2288.a2.core;
+    exports com.jackgharris.cosc2288.a2.controllers;
+    opens com.jackgharris.cosc2288.a2.core to javafx.fxml;
+    opens com.jackgharris.cosc2288.a2.controllers to javafx.fxml;
+}
