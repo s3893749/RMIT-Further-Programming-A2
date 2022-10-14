c3b94a4f3b441b3dded3141d6b14e4b514ec79fc
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-15 09:57:30 +1100

    - Created ThemeSettingsController to provide logic calls for the user defined theme settings.

---
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/ThemeSettingsController.java
@@ -0,0 +1,50 @@
+package com.jackgharris.cosc2288.a2.controllers;
+
+import com.jackgharris.cosc2288.a2.core.MyHealth;
+import com.jackgharris.cosc2288.a2.utility.ColorUtility;
+import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ThemeSettingsController {
+
+    @FXML
+    private ColorPicker backgroundColorPicker;
+    @FXML
+    private ColorPicker menuColorPicker;
+
+    @FXML
+    private AnchorPane settings;
+
+    @FXML
+    private HBox header;
+
+    @FXML
+    public void initialize(){
+        settings.setStyle("-fx-background-color: "+MyHealth.getTheme().getBackgroundColor());
+    }
+
+    public void backButton(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Preferences");
+        stage.setScene(FXMLUtility.loadScene(FXMLUtility.settingsFXML,stage, MyHealth.appCSS));
+        stage.show();
+    }
+
+    public void updateBackgroundColor(ActionEvent event){
+        MyHealth.getTheme().setBackgroundColor(ColorUtility.getHexStringFromColorPicker(this.backgroundColorPicker));
+        settings.setStyle("-fx-background-color: "+MyHealth.getTheme().getBackgroundColor());
+    }
+
+    public void updateMenuColor(ActionEvent event){
+        MyHealth.getTheme().setMenuColor(ColorUtility.getHexStringFromColorPicker(this.menuColorPicker));
+        header.setStyle("-fx-background-color: "+MyHealth.getTheme().getMenuColor());
+    }
+}
