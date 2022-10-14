3e34c0c892ff29e979f5b40179da8db80a4d00cc
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-15 09:59:21 +1100

    - Removed test user Object data, added and set new theme objerct. - Created new Database object for testing.

--- src/main/java/com/jackgharris/cosc2288/a2/core/MyHealth.java
+++ src/main/java/com/jackgharris/cosc2288/a2/core/MyHealth.java
@@ -1,37 +1,31 @@
 package com.jackgharris.cosc2288.a2.core;
 
-import com.jackgharris.cosc2288.a2.models.User;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
 import com.jackgharris.cosc2288.a2.utility.Resource;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Objects;
 
 public class MyHealth extends javafx.application.Application {
+
+
 
     public static final String launcherCSS = Objects.requireNonNull(MyHealth.class.getResource("login.css")).toExternalForm();
     public static final String appCSS = Objects.requireNonNull(MyHealth.class.getResource("app.css")).toExternalForm();
 
     public static final String title = "My Health v0.1";
 
+    private static Theme theme;
+
+
 
     public MyHealth(){
         System.out.println("MyHealth object created!");
-        HashMap<String, String> userData = new HashMap<>();
-        userData.put("username","st_tuff");
-        userData.put("email","st_tuff@me.com");
-        userData.put("password","Pa55w0rd");
-        userData.put("firstname","Jack");
-        userData.put("surname","Harris");
-        userData.put("profileImage","BLANK");
+        MyHealth.theme = new Theme("#b463ae",null,null,null,null);
 
-        User[] users = new User[1];
-        users[0] = new User(userData);
-        User.bindUserData(users);
-
+        Database.table("test");
     }
 
     @Override
@@ -50,5 +44,9 @@ public class MyHealth extends javafx.application.Application {
     public static void main(String[] args) {
 
         launch();
+    }
+
+    public static Theme getTheme(){
+        return MyHealth.theme;
     }
 }
