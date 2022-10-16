488215878e5c791bbfb37cb23efc4a27a4cb6b5c
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-16 17:32:01 +1100

    - Added initialize method call and auto application of the user defined styling.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/TerminalSettingsController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/TerminalSettingsController.java
@@ -3,13 +3,28 @@ package com.jackgharris.cosc2288.a2.controllers;
 import com.jackgharris.cosc2288.a2.core.MyHealth;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class TerminalSettingsController {
 
+    @FXML
+    private HBox header;
+
+    @FXML
+    private AnchorPane settings;
+
+    @FXML
+    public void initialize(){
+        settings.setStyle("-fx-background-color: "+MyHealth.getTheme().getBackgroundColor());
+        header.setStyle("-fx-background-color: "+MyHealth.getTheme().getMenuColor());
+    }
 
     public void backButton(ActionEvent event) throws IOException {
         Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
