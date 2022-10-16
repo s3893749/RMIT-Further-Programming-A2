3844440d1031522f9cefb601e653a92f6491f182
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-16 21:14:43 +1100

    - Added showSettingsMenu() method to check if a settings menu is open, and if not open it. In the event its open then the app will give the other window focus. - Added initialize check to set the background and menu color.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/DashboardController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/DashboardController.java
@@ -4,34 +4,42 @@ import com.jackgharris.cosc2288.a2.core.MyHealth;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
 import com.jackgharris.cosc2288.a2.utility.Resource;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.Menu;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class DashboardController {
 
     @FXML
-    private Menu logoutButton;
+    private AnchorPane parent;
 
-    public void logout(ActionEvent event) throws IOException {
-        Stage dashboardStage = (Stage)this.logoutButton.getParentPopup().getScene().getWindow();
-        Stage launcherStage = new Stage(StageStyle.UNDECORATED);
-        launcherStage.setScene(FXMLUtility.loadScene(FXMLUtility.loginFXML,launcherStage, MyHealth.launcherCSS));
-        launcherStage.show();
-        dashboardStage.hide();
+    @FXML
+    private VBox menu;
+
+    @FXML
+    public void initialize(){
+        parent.setStyle("-fx-background-color: "+MyHealth.getTheme().getBackgroundColor());
+        menu.setStyle("-fx-background-color: "+MyHealth.getTheme().getMenuColor());
     }
 
     public void showSettingsMenu(ActionEvent event) throws IOException {
-        Stage stage = new Stage();
-        stage.setResizable(false);
-        stage.getIcons().add(Resource.settingsFavicon());
-        stage.setScene(FXMLUtility.loadScene(FXMLUtility.settingsFXML,stage,MyHealth.appCSS));
-        stage.show();
+
+        if(!MyHealth.isStageShown("settings")){
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.getIcons().add(Resource.settingsFavicon());
+            stage.setTitle("Preferences");
+            stage.setScene(FXMLUtility.loadScene(FXMLUtility.settingsFXML,stage,MyHealth.appCSS));
+            stage.show();
+            stage.getProperties().put("id", "settings");
+        }else{
+            Stage stage = MyHealth.getStageById("settings");
+            stage.requestFocus();
+        }
+
+
     }
 }
