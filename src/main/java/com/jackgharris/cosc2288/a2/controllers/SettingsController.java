2e02c93f72e4c71517631103c5332d57e64876f1
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-16 17:33:01 +1100

    - Updated SettingsController to use new user theme colors.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/SettingsController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/SettingsController.java
@@ -3,12 +3,27 @@ package com.jackgharris.cosc2288.a2.controllers;
 import com.jackgharris.cosc2288.a2.core.MyHealth;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SettingsController {
+
+    @FXML
+    private AnchorPane settings;
+
+    @FXML
+    private HBox header;
+
+    @FXML
+    public void initialize(){
+        settings.setStyle("-fx-background-color: "+MyHealth.getTheme().getBackgroundColor());
+        header.setStyle("-fx-background-color: "+MyHealth.getTheme().getMenuColor());
+    }
 
     public void showTerminal(ActionEvent event) throws IOException {
         Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
