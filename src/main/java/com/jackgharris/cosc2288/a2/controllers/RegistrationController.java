55d870cc1daea895f108724d658dfca16f498de9
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-16 17:34:16 +1100

    - Added setPictureButtonPress() method, currently just shows the error message. - Updated registerButtonPress() to now show the error and highlight the error'd fields. - Added variables for all the user input fields.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/RegistrationController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/RegistrationController.java
@@ -3,26 +3,62 @@ package com.jackgharris.cosc2288.a2.controllers;
 import com.jackgharris.cosc2288.a2.core.MyHealth;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class RegistrationController {
 
+    @FXML
+    TextField firstNameInputField;
 
-    public RegistrationController(){
-        System.out.println("Registration Controller Created!");
-    }
+    @FXML
+    TextField lastNameInputField;
 
-    public void backButton(ActionEvent event) throws IOException {
+    @FXML
+    TextField emailInputField;
+
+    @FXML
+    TextField usernameInputField;
+
+    @FXML
+    TextField passwordInputField;
+
+    @FXML
+    TextField passwordConfirmationInputField;
+
+    @FXML
+    Label registrationFailedError;
+
+
+
+    public void backButtonPress(ActionEvent event) throws IOException {
         Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
         stage.setScene(FXMLUtility.loadScene(FXMLUtility.loginFXML,stage, MyHealth.launcherCSS));
         stage.show();
     }
 
-    public void registerButton(ActionEvent event){
+    public void registerButtonPress(ActionEvent event){
         System.out.println("Register Button Pressed!");
+        this.registrationFailedError.setText("Registration function not yet added");
 
+
+        this.registrationFailedError.getStyleClass().remove("notification-hidden");
+        this.firstNameInputField.getStyleClass().add("text-field-error");
+        this.lastNameInputField.getStyleClass().add("text-field-error");
+        this.emailInputField.getStyleClass().add("text-field-error");
+        this.usernameInputField.getStyleClass().add("text-field-error");
+        this.passwordInputField.getStyleClass().add("text-field-error");
+        this.passwordConfirmationInputField.getStyleClass().add("text-field-error");
+
+    }
+
+    public void setPictureButtonPress(ActionEvent event){
+        this.registrationFailedError.getStyleClass().remove("notification-hidden");
+        this.registrationFailedError.setText("Set Picture feature not yet added");
     }
 }
