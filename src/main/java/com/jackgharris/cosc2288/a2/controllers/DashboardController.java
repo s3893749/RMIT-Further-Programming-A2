bdbe1e412f84d7c2cd2df1b15efe748da19dc204
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-28 12:06:47 +1100

    - Added method to add a callback that disables all the currently used dates from being selected again for this record type. - Disabled all the add record inputs by default, these will now be unlocked you enter the value in sequence. - Created dateSelectedAction and valueEnteredAction, these methods will now reename the next step. - Updated AddNewRecord method to now null/reset the selected date and the weight input, it will also disable its self and the weight input button. - Created deleteRecordButton, this creates our deleteRecordPopup model and confirms the delete request is valid.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/DashboardController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/DashboardController.java
@@ -1,20 +1,25 @@
 package com.jackgharris.cosc2288.a2.controllers;
 
 import com.jackgharris.cosc2288.a2.core.MyHealth;
+import com.jackgharris.cosc2288.a2.models.Record;
 import com.jackgharris.cosc2288.a2.models.Weight;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
 import com.jackgharris.cosc2288.a2.utility.Resource;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class DashboardController {
 
@@ -25,7 +30,7 @@ public class DashboardController {
     private VBox menu;
 
     @FXML
-    private LineChart<String, Integer> testChart;
+    private LineChart<String, Float> testChart;
 
     @FXML
     private TableView<Weight> weightTable;
@@ -34,7 +39,7 @@ public class DashboardController {
     private TableColumn<Weight, String> dateColumn;
 
     @FXML
-    private TableColumn<Weight, Integer> weightColumn;
+    private TableColumn<Weight, Float> weightColumn;
 
     @FXML
     private DatePicker addRecordDatePicker;
@@ -51,15 +56,48 @@ public class DashboardController {
     @FXML
     private Button editRecordButton;
 
+    private Callback<DatePicker, DateCell> disableUsedDates() {
+        final Callback<DatePicker, DateCell> dayCellFactory = (final DatePicker datePicker) -> new DateCell() {
+            @Override
+            public void updateItem(LocalDate item, boolean empty) {
+                super.updateItem(item, empty);
+                // Disable Monday
+
+                for (Record record: Weight.getAll()){
+                    if(item.equals(record.getDate())){
+                        setDisable(true);
+                        setStyle("-fx-background-color: #c72929;");
+                        setCursor(Cursor.CROSSHAIR);
+                    }
+                }
+            }
+        };
+        return dayCellFactory;
+    }
+
     @FXML
     public void initialize(){
         parent.setStyle("-fx-background-color: "+MyHealth.getTheme().getBackgroundColor());
         menu.setStyle("-fx-background-color: "+MyHealth.getTheme().getMenuColor());
 
         this.updateRecordTable();
-
         this.addRecordDatePicker.getEditor().setDisable(true);
         this.updateTestChart();
+
+
+        Callback<DatePicker, DateCell> dayCellFactory  = this.disableUsedDates();
+
+        this.addRecordDatePicker.setDayCellFactory(dayCellFactory);
+        this.addRecordButton.setDisable(true);
+        this.addRecordWeightInput.setDisable(true);
+    }
+
+    public void dateSelectedAction(){
+        this.addRecordWeightInput.setDisable(false);
+    }
+
+    public void valueEnteredAction(){
+        this.addRecordButton.setDisable(false);
     }
 
     public void showSettingsMenu(ActionEvent event) throws IOException {
@@ -82,10 +120,10 @@ public class DashboardController {
         System.out.println("Updating Chart");
         //System.out.println(this.startDate.getValue());
         this.testChart.getData().clear();
-        XYChart.Series<String, Integer> series = new XYChart.Series<>();
+        XYChart.Series<String, Float> series = new XYChart.Series<>();
 
         for (Weight weight : Weight.getAll(123)){
-            series.getData().add(new XYChart.Data<>(weight.getDate(), weight.getValue()));
+            series.getData().add(new XYChart.Data<>(weight.getDate().toString(), weight.getValue()));
         }
         this.testChart.setLegendVisible(false);
         this.testChart.getData().add(series);
@@ -99,15 +137,36 @@ public class DashboardController {
     }
 
     public void addNewRecord(ActionEvent event){
-        Weight.add(new Weight(123,Integer.parseInt(addRecordWeightInput.getText()),this.addRecordDatePicker.getValue().toString()));
+        Weight.add(new Weight(123,Float.parseFloat(addRecordWeightInput.getText()),this.addRecordDatePicker.getValue().toString()));
+        this.addRecordDatePicker.setValue(null);
+        this.addRecordWeightInput.setText(null);
+        this.addRecordButton.setDisable(true);
+        this.addRecordWeightInput.setDisable(true);
         this.updateTestChart();
         this.updateRecordTable();
     }
 
-    public void deleteRecord(ActionEvent event){
+    public void deleteRecord(ActionEvent event) throws IOException {
        Weight weight = this.weightTable.getSelectionModel().getSelectedItem();
 
-       Weight.remove(weight);
+        MyHealth.data.put("record", weight);
+
+        if(!MyHealth.isStageShown("deleteRecordPopup")){
+            Stage stage = new Stage();
+            stage.getProperties().put("id", "deleteRecordPopup");
+            stage.setResizable(false);
+            stage.getIcons().add(Resource.warningFavicon());
+            stage.setTitle("Delete Record Confirmation");
+            stage.setScene(FXMLUtility.loadScene(FXMLUtility.deleteRecordPopup,stage,MyHealth.appCSS));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initOwner(MyHealth.getStageById("dashboard"));
+            stage.showAndWait();
+
+        }else{
+            Stage stage = MyHealth.getStageById("deleteRecordPopup");
+            stage.requestFocus();
+        }
+
        this.updateRecordTable();
        this.updateTestChart();
     }
