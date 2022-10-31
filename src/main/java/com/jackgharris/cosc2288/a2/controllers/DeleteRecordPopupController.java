f2b5d5d890c1163ceb68baf9a4a56611d28e31dd
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-31 18:26:39 +1100

    - Updated DeleteRecordPopupController to now implement the HasActiveRecord interface.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/DeleteRecordPopupController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/DeleteRecordPopupController.java
@@ -1,13 +1,14 @@
 package com.jackgharris.cosc2288.a2.controllers;
 
 import com.jackgharris.cosc2288.a2.core.MyHealth;
+import com.jackgharris.cosc2288.a2.interfaces.HasActiveRecord;
 import com.jackgharris.cosc2288.a2.models.Record;
 import com.jackgharris.cosc2288.a2.models.Weight;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
-public class DeleteRecordPopupController {
+public class DeleteRecordPopupController implements HasActiveRecord {
 
     @FXML
     private Label recordType;
@@ -35,10 +36,16 @@ public class DeleteRecordPopupController {
         MyHealth.getStageById("deleteRecordPopup").close();
     }
 
+    @Override
     public void setRecord(Record record){
         this.record = record;
         this.recordType.setText("Type: "+this.record.getClass().getSimpleName());
         this.recordDate.setText("Date: "+this.record.getDate());
         this.recordValue.setText("Value: "+this.record.getValue());
+    }
+
+    @Override
+    public Record getRecord() {
+        return this.record;
     }
 }
