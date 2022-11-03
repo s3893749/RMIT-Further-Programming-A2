c8b18d449a0bc912821727d6ad5aca2a8e25767a
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-03 14:19:20 +1100

    - Updated the module-info.java to now export the controller -> components package. - Updated the module-info.java to now open the controller -> components package to javafx.fxml.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,8 +8,10 @@ module com.jackgharris.cosc2288.a2 {
     exports com.jackgharris.cosc2288.a2.core;
     exports com.jackgharris.cosc2288.a2.controllers;
     exports com.jackgharris.cosc2288.a2.models;
+    exports com.jackgharris.cosc2288.a2.controllers.components;
 
     opens com.jackgharris.cosc2288.a2.models to javafx.base;
     opens com.jackgharris.cosc2288.a2.core to javafx.fxml;
     opens com.jackgharris.cosc2288.a2.controllers to javafx.fxml;
+    opens com.jackgharris.cosc2288.a2.controllers.components to javafx.fxml;
 }
