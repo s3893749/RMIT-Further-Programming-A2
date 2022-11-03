e40af6068adeddb5d8322f16d62595d8317ba5e1
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-03 14:43:51 +1100

    - Created new RecordCollection object, this is returned from the Record.Where(String key, String value) call and returns the collectionObject that can be used to chain querys and get data. - Added getAndCache() method, this returns the data only from the cache and will set it if requried.

---
+++ src/main/java/com/jackgharris/cosc2288/a2/models/RecordCollection.java
@@ -0,0 +1,60 @@
+package com.jackgharris.cosc2288.a2.models;
+
+import com.jackgharris.cosc2288.a2.core.Database;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+public class RecordCollection {
+
+    private String query;
+    private boolean whereCalled;
+
+    public RecordCollection(){
+        this.query = "SELECT * FROM records";
+        this.whereCalled = false;
+    }
+
+    public RecordCollection where(String key, String value){
+
+        if(!this.whereCalled) {
+            this.query += " WHERE " + key + "='" + value + "'";
+            this.whereCalled = true;
+        }else{
+            this.query += " AND "  + key + "='" + value + "'";
+        }
+        return this;
+    }
+
+
+    public ObservableList<Record> get(){
+        System.out.println(this.query);
+
+        ObservableList<Record> output = FXCollections.observableArrayList();
+
+        Vector<HashMap<String, String>> resultSet = Database.query(this.query);
+
+        resultSet.forEach((n) ->{
+            output.add(new Record(
+                    Integer.parseInt(n.get("id")),
+                    n.get("type"),
+                    Integer.parseInt(n.get("user_id")),
+                    n.get("value"),
+                    n.get("date")));
+        });
+
+        return output;
+    }
+
+    public ObservableList<Record> getAndCache(){
+
+        if (Record.getCache() == null) {
+            Record.setCache(this.get());
+        }
+
+        return Record.getCache();
+    }
+
+}
