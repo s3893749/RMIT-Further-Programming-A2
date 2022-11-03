56285b7c795709307ccd35420a07b22d0b0b4254
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-03 14:13:35 +1100

    - Created static method GetUserByEmail(String email), this returns the user by object for that email. - Created static method login(String email, String password), this returns true or false depending on if the email and password are valid. - Updated the user class to now use private variables instead of a hashmap of values.

--- src/main/java/com/jackgharris/cosc2288/a2/models/User.java
+++ src/main/java/com/jackgharris/cosc2288/a2/models/User.java
@@ -1,53 +1,84 @@
 package com.jackgharris.cosc2288.a2.models;
 
-import java.util.ArrayList;
+
+import com.jackgharris.cosc2288.a2.core.Database;
+
 import java.util.HashMap;
+import java.util.Vector;
 
 public class User {
 
-    private final HashMap<String, String> data;
 
-    private static User[] users;
+    private int id;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
 
-    public User(HashMap<String, String> data){
-        this.data = data;
+    public User(int id, String username, String firstname, String lastname, String email, String password){
+        this.id = id;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
     }
 
     public String getUsername(){
-        return this.data.get("username");
+        return this.username;
     }
 
     public String getFirstname(){
-        return this.data.get("firstname");
+        return this.firstname;
     }
 
     public String getSurname(){
-        return this.data.get("surname");
+        return this.lastname;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPassword(){
+        return this.password;
     }
 
     public String getProfileImage(){
-        return this.data.get("profileImage");
+        return null;
     }
 
-    public String getValueWhereKey(String key){
-        return this.data.get(key);
+    public int getId(){
+        return this.id;
     }
 
-    public static User[] where(String key, String value){
 
-        ArrayList<User> searchResults = new ArrayList<>();
+    public static boolean login(String email, String password){
+        String query = "SELECT * FROM users WHERE email='"+email+"' AND password='"+password+"'";
 
-        for(User user: User.users){
-            if(user.getValueWhereKey(key).equals(value)){
-                searchResults.add(user);
-            }
-        }
+        Vector<HashMap<String,String>> result =  Database.query(query);
 
-        return searchResults.toArray(User[]::new);
+        boolean outcome = false;
+
+        outcome = !result.isEmpty();
+
+        return outcome;
+
     }
 
-    public static void bindUserData(User[] users){
-        User.users = users;
+    public static User getByEmail(String email){
+        String query = "SELECT * FROM users WHERE email='"+email+"'";
+
+        Vector<HashMap<String ,String>> data = Database.query(query);
+
+        return new User(
+                Integer.parseInt(data.get(0).get("id")),
+                data.get(0).get("username"),
+                data.get(0).get("firstname"),
+                data.get(0).get("lastname"),
+                data.get(0).get("email"),
+                data.get(0).get("password"));
     }
 
 
