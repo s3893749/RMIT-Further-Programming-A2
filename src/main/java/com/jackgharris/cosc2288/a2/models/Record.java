d0234e2d47617e6cff774582d2512632d42ac3ed
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-03 14:42:35 +1100

    - Updated Record model to now store all the values that are inside the database. - Added required getters and setters for it. - Added static getter to map directly to the where method of the RecordCollection object. - Added static getter and setter for the new ObervableArray cache. - Added static setter for Add to add a new record to the database -> functionality not yet added.

--- src/main/java/com/jackgharris/cosc2288/a2/models/Record.java
+++ src/main/java/com/jackgharris/cosc2288/a2/models/Record.java
@@ -1,14 +1,22 @@
 package com.jackgharris.cosc2288.a2.models;
 
+import com.jackgharris.cosc2288.a2.core.MyHealth;
+import javafx.collections.ObservableList;
+
 import java.time.LocalDate;
 
 public class Record {
 
-    protected int userId;
-    protected Float value;
-    protected String date;
+    private int id;
+    private final String type;
+    private final int userId;
+    private String value;
+    private String date;
+    private static ObservableList<Record> cache;
 
-    public Record(int userId, Float value, String date){
+    public Record(int id, String type, int userId, String value, String date){
+        this.id = id;
+        this.type = type;
         this.userId = userId;
         this.value = value;
         this.date = date;
@@ -18,7 +26,7 @@ public class Record {
         return this.userId;
     }
 
-    public Float getValue(){
+    public String getValue(){
         return this.value;
     }
 
@@ -31,7 +39,33 @@ public class Record {
     }
 
     public void setValue(Float value){
-        this.value = value;
+        this.value = String.valueOf(value);
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public static RecordCollection where(String key, String value){
+        return new RecordCollection().where(key, value);
+    }
+
+    public static void setCache(ObservableList<Record> cache){
+        Record.cache = cache;
+    }
+
+    public static ObservableList<Record> getCache(){
+        return Record.cache;
+    }
+
+    public static void add(Record record){
+
+        String sql = "INSERT INTO records (type, user_id, value, date) VALUES ('"+record.getType()+"','"+ MyHealth.getInstance().getUser().getId()+"', '"+record.getValue()+"','"+record.getDate()+"')";
+        System.out.println(sql);
     }
 
 }
