f87e75419c90255cc60dc2e2a7e3165d704dc08a
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-05 17:35:55 +1100

    - Removed unused methods from the DashboardController -> "dateSelectionAction, valueEnteredAction, showSettingsMenu, updateTestChart, updateRecordTable, addNewRecord".  These methods are now part of new lineChartRecord fxml file and controller.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/DashboardController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/DashboardController.java
@@ -1,27 +1,20 @@
 package com.jackgharris.cosc2288.a2.controllers;
 
+import com.jackgharris.cosc2288.a2.controllers.components.popups.DeleteRecordPopupController;
+import com.jackgharris.cosc2288.a2.controllers.components.popups.EditRecordPopupController;
 import com.jackgharris.cosc2288.a2.core.MyHealth;
-import com.jackgharris.cosc2288.a2.models.Record;
 import com.jackgharris.cosc2288.a2.models.Weight;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
 import com.jackgharris.cosc2288.a2.utility.Resource;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
 import java.io.IOException;
-import java.time.LocalDate;
 
 public class DashboardController{
 
@@ -29,123 +22,25 @@ public class DashboardController{
     private AnchorPane parent;
 
     @FXML
-    private VBox menu;
+    private AnchorPane menuContainerOuter;
 
-    @FXML
-    private LineChart<String, Float> testChart;
 
     @FXML
     private TableView<Weight> weightTable;
 
-    @FXML
-    private TableColumn<Weight, String> dateColumn;
 
     @FXML
-    private TableColumn<Weight, Float> weightColumn;
+    public void initialize() throws IOException {
+        //Load the menu
+        FXMLLoader menuLoader = new FXMLLoader(FXMLUtility.menu);
+        AnchorPane menuNodes = menuLoader.load();
+        this.menuContainerOuter.getChildren().add(menuNodes.getChildren().get(0));
 
-    @FXML
-    private DatePicker addRecordDatePicker;
-
-    @FXML
-    private TextField addRecordWeightInput;
-
-    @FXML
-    private Button addRecordButton;
-
-
-    private Callback<DatePicker, DateCell> disableUsedDates() {
-        final Callback<DatePicker, DateCell> dayCellFactory = (final DatePicker datePicker) -> new DateCell() {
-            @Override
-            public void updateItem(LocalDate item, boolean empty) {
-                super.updateItem(item, empty);
-                // Disable Monday
-
-                for (Record record: Weight.getAll()){
-                    if(item.equals(record.getDate())){
-                        setDisable(true);
-                        setStyle("-fx-background-color: #c72929;");
-                        setCursor(Cursor.CROSSHAIR);
-                    }
-                }
-            }
-        };
-        return dayCellFactory;
+        //set the background colors
+        this.menuContainerOuter.setStyle("-fx-background-color: "+MyHealth.getInstance().getTheme().getMenuColor());
+        this.parent.setStyle("-fx-background-color: "+MyHealth.getInstance().getTheme().getBackgroundColor());
     }
 
-    @FXML
-    public void initialize(){
-        this.parent.setStyle("-fx-background-color: "+MyHealth.getTheme().getBackgroundColor());
-        this.menu.setStyle("-fx-background-color: "+MyHealth.getTheme().getMenuColor());
-
-        this.updateRecordTable();
-        this.addRecordDatePicker.getEditor().setDisable(true);
-        this.updateTestChart();
-
-
-        Callback<DatePicker, DateCell> dayCellFactory  = this.disableUsedDates();
-
-        this.addRecordDatePicker.setDayCellFactory(dayCellFactory);
-        this.addRecordButton.setDisable(true);
-        this.addRecordWeightInput.setDisable(true);
-
-        this.weightTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        this.dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
-        this.weightColumn.setCellValueFactory(new PropertyValueFactory<>("value"));
-        this.weightTable.setItems(Weight.getAll(123));
-
-        this.weightTable.refresh();
-    }
-
-    public void dateSelectedAction(){
-        this.addRecordWeightInput.setDisable(false);
-    }
-
-    public void valueEnteredAction(){
-        this.addRecordButton.setDisable(false);
-    }
-
-    public void showSettingsMenu() throws IOException {
-
-        if(!MyHealth.isStageShown("settings")){
-            Stage stage = new Stage();
-            stage.setResizable(false);
-            stage.getIcons().add(Resource.settingsFavicon());
-            stage.setTitle("Preferences");
-            stage.setScene(FXMLUtility.loadScene(FXMLUtility.settingsFXML,stage,MyHealth.appCSS));
-            stage.show();
-            stage.getProperties().put("id", "settings");
-        }else{
-            Stage stage = MyHealth.getStageById("settings");
-            stage.requestFocus();
-        }
-    }
-
-    public void updateTestChart(){
-        this.testChart.getData().clear();
-        XYChart.Series<String, Float> series = new XYChart.Series<>();
-
-        for (Weight weight : Weight.getAll(123)){
-            series.getData().add(new XYChart.Data<>(weight.getDate().toString(), weight.getValue()));
-        }
-
-        this.testChart.setLegendVisible(false);
-        this.testChart.getData().add(series);
-    }
-
-    public void updateRecordTable(){
-        this.weightTable.getItems().clear();
-        this.weightTable.getItems().addAll(Weight.getAll(123));
-    }
-
-    public void addNewRecord(ActionEvent event){
-        Weight.add(new Weight(123,Float.parseFloat(addRecordWeightInput.getText()),this.addRecordDatePicker.getValue().toString()));
-        this.addRecordDatePicker.setValue(null);
-        this.addRecordWeightInput.setText(null);
-        this.addRecordButton.setDisable(true);
-        this.addRecordWeightInput.setDisable(true);
-        this.updateTestChart();
-        this.updateRecordTable();
-    }
 
     public void deleteRecord() throws IOException {
        Weight weight = this.weightTable.getSelectionModel().getSelectedItem();
@@ -172,9 +67,6 @@ public class DashboardController{
             Stage stage = MyHealth.getStageById("deleteRecordPopup");
             stage.requestFocus();
         }
-
-       this.updateRecordTable();
-       this.updateTestChart();
     }
 
     public void editRecord() throws IOException {
@@ -205,8 +97,7 @@ public class DashboardController{
             stage.requestFocus();
         }
 
-        this.updateRecordTable();
-        this.updateTestChart();
-
     }
+
+
 }
