e81084d5afc852bb3ea8d62b94178aa24c324fbc
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-08 16:00:14 +1100

    - Updated login in the registration controller to now correctly set the profile picture and register the user!

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/RegistrationController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/RegistrationController.java
@@ -1,15 +1,20 @@
 package com.jackgharris.cosc2288.a2.controllers;
 
+import com.jackgharris.cosc2288.a2.core.EasyImage;
 import com.jackgharris.cosc2288.a2.core.MyHealth;
+import com.jackgharris.cosc2288.a2.models.User;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
 
 public class RegistrationController {
 
@@ -34,31 +39,36 @@ public class RegistrationController {
     @FXML
     Label registrationFailedError;
 
+    @FXML
+    ImageView photoPreview;
+
 
 
     public void backButtonPress(ActionEvent event) throws IOException {
         Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
         stage.setScene(FXMLUtility.loadScene(FXMLUtility.loginFXML,stage, MyHealth.launcherCSS));
+        stage.getProperties().put("id","launcher");
         stage.show();
     }
 
     public void registerButtonPress(ActionEvent event){
         System.out.println("Register Button Pressed!");
-        this.registrationFailedError.setText("Registration function not yet added");
 
+        User user = new User(0,usernameInputField.getText(), firstNameInputField.getText(),lastNameInputField.getText(),emailInputField.getText(), passwordInputField.getText(), EasyImage.serialize(new EasyImage(this.photoPreview.getImage())));
 
-        this.registrationFailedError.getStyleClass().remove("notification-hidden");
-        this.firstNameInputField.getStyleClass().add("text-field-error");
-        this.lastNameInputField.getStyleClass().add("text-field-error");
-        this.emailInputField.getStyleClass().add("text-field-error");
-        this.usernameInputField.getStyleClass().add("text-field-error");
-        this.passwordInputField.getStyleClass().add("text-field-error");
-        this.passwordConfirmationInputField.getStyleClass().add("text-field-error");
+        User.add(user);
 
     }
 
     public void setPictureButtonPress(ActionEvent event){
-        this.registrationFailedError.getStyleClass().remove("notification-hidden");
-        this.registrationFailedError.setText("Set Picture feature not yet added");
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Please select your profile picture!");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Portable Network Graphics","*.png"));
+        File photo = fileChooser.showOpenDialog(MyHealth.getStageById("registration"));
+
+        EasyImage selectedImage =  new EasyImage(new Image(photo.getAbsolutePath(),200,200, false, true));
+
+        this.photoPreview.setImage(selectedImage.getImage());
     }
 }
