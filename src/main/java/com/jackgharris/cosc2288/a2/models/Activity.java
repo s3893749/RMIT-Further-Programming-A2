b9ea70a5008ddcb05a75464062986d815e33be16
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-09 10:13:19 +1100

    - Updated Activity class to now only accept the description, or description + time in the case it's loaded from the database. - Created Add method that accepts an activity and saves it to the database. - Created get method to return the activities for a specific user, this method also accepts a limit.

--- src/main/java/com/jackgharris/cosc2288/a2/models/Activity.java
+++ src/main/java/com/jackgharris/cosc2288/a2/models/Activity.java
@@ -1,27 +1,57 @@
 package com.jackgharris.cosc2288.a2.models;
 
+import com.jackgharris.cosc2288.a2.core.Database;
+import com.jackgharris.cosc2288.a2.core.MyHealth;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Vector;
+
 public class Activity {
 
-    private String caller;
-    private String description;
-    private String userName;
+    private final String description;
+    private final String time;
 
-    public Activity(Object caller, User user ,String description){
-        this.caller = caller.getClass().getName();
+    public Activity(String description){
         this.description = description;
-        this.userName = user.getFirstname()+" "+user.getSurname();
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+        Date date = new Date();
+        this.time = formatter.format(date);
     }
 
-    public String getCaller(){
-        return this.caller;
+    public Activity(String description, String time){
+        this.description = description;
+        this.time = time;
     }
+
 
     public String getDescription(){
         return this.description;
     }
 
-    public String getUserName(){
-        return this.userName;
+
+    public String getTime(){
+        return this.time;
+    }
+
+    public static boolean add(Activity activity){
+        String sql = "INSERT INTO activities (user_id, description, time) VALUES ('"+MyHealth.getInstance().getUser().getId()+"', '"+activity.getDescription()+"','"+activity.getTime()+"')";
+
+        return Database.queryWithBooleanResult(sql);
+    }
+
+    public static ArrayList<Activity> get(int limit){
+        ArrayList<Activity> output = new ArrayList<>();
+
+        String sql = "SELECT * FROM activities WHERE user_id='"+MyHealth.getInstance().getUser().getId()+"' LIMIT "+limit;
+
+        Vector<HashMap<String, String>> result = Database.query(sql);
+        result.forEach((n)->{
+            output.add(new Activity(n.get("description"), n.get("time")));
+        });
+        return output;
     }
 
 }
