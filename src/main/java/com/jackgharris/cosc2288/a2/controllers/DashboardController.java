959771e9f0a1ed193a5b77148fee4ae3e17d9d76
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-08 15:59:41 +1100

    - Removed all old import & FXML references from the DashboardController.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/DashboardController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/DashboardController.java
@@ -1,18 +1,10 @@
 package com.jackgharris.cosc2288.a2.controllers;
 
-import com.jackgharris.cosc2288.a2.controllers.components.popups.DeleteRecordPopupController;
-import com.jackgharris.cosc2288.a2.controllers.components.popups.EditRecordPopupController;
 import com.jackgharris.cosc2288.a2.core.MyHealth;
-import com.jackgharris.cosc2288.a2.models.Weight;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
-import com.jackgharris.cosc2288.a2.utility.Resource;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -23,10 +15,6 @@ public class DashboardController{
 
     @FXML
     private AnchorPane menuContainerOuter;
-
-
-    @FXML
-    private TableView<Weight> weightTable;
 
 
     @FXML
