77e284bbcec3b415eb4ebc5cfc6019b459453f9f
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-08 11:00:12 +1100

    - Rebuild RecordCollection.where() method to now store all the values as a key value pair, then add them in as required. - Updated RecordCollection.get() method to now factor in if updateCache() or fromCache() are called to determine if it needs to update the cache or return values from it instead of SQL.

--- src/main/java/com/jackgharris/cosc2288/a2/models/RecordCollection.java
+++ src/main/java/com/jackgharris/cosc2288/a2/models/RecordCollection.java
@@ -1,40 +1,52 @@
 package com.jackgharris.cosc2288.a2.models;
 
 import com.jackgharris.cosc2288.a2.core.Database;
+import com.jackgharris.cosc2288.a2.core.MyHealth;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.util.HashMap;
 import java.util.Vector;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class RecordCollection {
 
     private String query;
-    private boolean whereCalled;
-    private boolean updateCacheCalled;
+    private boolean shouldUpdateCache;
     private String orderBy;
+    private final HashMap<String, String> where;
+    private int limit;
+    private boolean shouldUseCache;
 
     public RecordCollection(){
         this.query = "SELECT * FROM records";
-        this.whereCalled = false;
-        this.updateCacheCalled = false;
+        this.shouldUpdateCache = false;
         this.orderBy = "";
+        this.where = new HashMap<>();
+        this.limit = 0;
     }
 
     public RecordCollection where(String key, String value){
 
-        if(!this.whereCalled) {
-            this.query += " WHERE " + key + "='" + value + "'";
-            this.whereCalled = true;
-        }else{
-            this.query += " AND "  + key + "='" + value + "'";
-        }
+        this.where.put(key,value);
+
         return this;
     }
 
     public RecordCollection updateCache(){
-        Record.setCache(this.get());
-        this.updateCacheCalled = true;
+        this.shouldUpdateCache = true;
+        return this;
+    }
+
+    public RecordCollection fromCache(){
+        this.shouldUseCache = true;
+        return this;
+    }
+
+    public RecordCollection withCurrentUser(){
+
+        this.where.put("user_id",String.valueOf(MyHealth.getInstance().getUser().getId()));
+
         return this;
     }
 
@@ -43,38 +55,58 @@ public class RecordCollection {
         return this;
     }
 
+    public RecordCollection limit(int limit){
+        this.limit = limit;
+        return this;
+    }
+
     public ObservableList<Record> get(){
+
+        if(this.shouldUseCache){
+            return Record.getCache();
+        }
+
+        AtomicInteger count = new AtomicInteger();
+        count.set(0);
+
+        this.where.forEach((key,value) ->{
+
+            if(count.get() == 0){
+                this.query += " WHERE "+key+"='"+value+"'";
+            }else{
+                this.query += " AND "+key+"='"+value+"'";
+
+            }
+            count.getAndIncrement();
+        });
 
         this.query += this.orderBy;
 
-        if(this.updateCacheCalled){
-            return Record.getCache();
-        }else {
+        if(this.limit > 0){
+        this.query += " LIMIT "+this.limit;
+        }
+
+
+        ObservableList<Record> output = FXCollections.observableArrayList();
+
+        Vector<HashMap<String, String>> resultSet = Database.query(this.query);
+
+        resultSet.forEach((n) -> {
+            output.add(new Record(
+                    Integer.parseInt(n.get("id")),
+                    n.get("type"),
+                    Integer.parseInt(n.get("user_id")),
+                    n.get("value"),
+                    n.get("date")));
+        });
+
+
+        if(this.shouldUpdateCache){
+            Record.setCache(output);
             System.out.println(this.query);
 
-            ObservableList<Record> output = FXCollections.observableArrayList();
-
-            Vector<HashMap<String, String>> resultSet = Database.query(this.query);
-
-            resultSet.forEach((n) -> {
-                output.add(new Record(
-                        Integer.parseInt(n.get("id")),
-                        n.get("type"),
-                        Integer.parseInt(n.get("user_id")),
-                        n.get("value"),
-                        n.get("date")));
-            });
-            return output;
-        }
-    }
-
-    public ObservableList<Record> getAndCache(){
-
-        if (Record.getCache() == null) {
-            Record.setCache(this.get());
         }
 
-        return Record.getCache();
+        return output;
     }
-
 }
