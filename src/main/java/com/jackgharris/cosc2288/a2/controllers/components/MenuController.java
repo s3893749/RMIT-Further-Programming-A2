f63a2e7e48f51f9f3eabc2ad1ec839e9f7dfad75
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-13 20:48:56 +1100

    - Updated MenuController to now change the color of the active button that was pressed. - Added code for preferencesButtonPress() method to correctly show the new settings page.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/components/MenuController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/components/MenuController.java
@@ -3,8 +3,12 @@ package com.jackgharris.cosc2288.a2.controllers.components;
 import com.jackgharris.cosc2288.a2.core.MyHealth;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
 import com.jackgharris.cosc2288.a2.utility.Resource;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -13,21 +17,34 @@ import java.io.IOException;
 
 public class MenuController {
 
-    public void preferencesButtonPress() throws IOException {
 
-        //if(!MyHealth.isStageShown("settings")){
-       //     Stage stage =  new Stage();
-       //     stage.getProperties().put("id","settings");
-       //     stage.setScene(FXMLUtility.loadScene(FXMLUtility.settingsFXML,stage,MyHealth.appCSS));
-       //     stage.setResizable(false);
-       //     stage.show();
-       // }else{
-        //    MyHealth.getStageById("settings").requestFocus();
-        //}
+    @FXML
+    private VBox menu;
+
+
+    public void preferencesButtonPress(ActionEvent event) throws IOException {
+
+        this.setActiveButton((Button) event.getSource());
+
+        Stage stage = MyHealth.getStageById("dashboard");
+        AnchorPane root = (AnchorPane) stage.getScene().getRoot();
+
+        AnchorPane contentContainerOuter = (AnchorPane) root.getChildren().get(1);
+        contentContainerOuter.getChildren().clear();
+
+
+        FXMLLoader loader = new FXMLLoader(FXMLUtility.settingsPage);
+        AnchorPane nodes = loader.load();
+
+        contentContainerOuter.getChildren().clear();
+        contentContainerOuter.getChildren().addAll(nodes.getChildren());
 
     }
 
-    public void switchToRecent() throws IOException {
+    public void switchToRecent(ActionEvent event) throws IOException {
+
+        this.setActiveButton((Button) event.getSource());
+
         Stage stage = MyHealth.getStageById("dashboard");
         AnchorPane root = (AnchorPane) stage.getScene().getRoot();
 
@@ -43,7 +60,11 @@ public class MenuController {
     }
 
 
-    public void switchToHealthRecord(){
+    public void switchToHealthRecord(ActionEvent event){
+
+        this.setActiveButton((Button) event.getSource());
+
+
         Stage stage = MyHealth.getStageById("dashboard");
         AnchorPane root = (AnchorPane) stage.getScene().getRoot();
 
@@ -51,7 +72,10 @@ public class MenuController {
         contentContainerOuter.getChildren().clear();
     }
 
-    public void switchToTemperature() throws IOException {
+    public void switchToTemperature(ActionEvent event) throws IOException {
+
+        this.setActiveButton((Button) event.getSource());
+
         Stage stage = MyHealth.getStageById("dashboard");
         AnchorPane root = (AnchorPane) stage.getScene().getRoot();
 
@@ -67,7 +91,8 @@ public class MenuController {
         contentContainerOuter.getChildren().addAll(nodes.getChildren());
     }
 
-    public void switchToWeight() throws IOException {
+    public void switchToWeight(ActionEvent event) throws IOException {
+        this.setActiveButton((Button) event.getSource());
 
         Stage stage = MyHealth.getStageById("dashboard");
         AnchorPane root = (AnchorPane) stage.getScene().getRoot();
@@ -85,7 +110,9 @@ public class MenuController {
 
     }
 
-    public void switchToBloodPressure() throws IOException {
+    public void switchToBloodPressure(ActionEvent event) throws IOException {
+        this.setActiveButton((Button) event.getSource());
+
         Stage stage = MyHealth.getStageById("dashboard");
         AnchorPane root = (AnchorPane) stage.getScene().getRoot();
 
@@ -114,6 +141,17 @@ public class MenuController {
         stage.getProperties().put("id","launcher");
 
         stage.show();
+    }
+
+    private void setActiveButton(Button button){
+
+        this.menu.getChildren().forEach((n)->{
+            if(n == button){
+                button.setStyle("-fx-background-color:  #b20844");
+            }else{
+                n.setStyle("");
+            }
+        });
     }
 
 }
