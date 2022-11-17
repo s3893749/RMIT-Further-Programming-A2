31a56e4c353727de48423b55c023d5b08da473e1
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-17 17:30:17 +1100

    - Updated module-info.java file.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,8 +10,8 @@ module com.jackgharris.cosc2288.a2 {
     exports com.jackgharris.cosc2288.a2.models;
     exports com.jackgharris.cosc2288.a2.controllers.components;
 
-    opens com.jackgharris.cosc2288.a2.models to javafx.base;
     opens com.jackgharris.cosc2288.a2.core to javafx.fxml;
     opens com.jackgharris.cosc2288.a2.controllers to javafx.fxml;
     opens com.jackgharris.cosc2288.a2.controllers.components to javafx.fxml;
+    opens com.jackgharris.cosc2288.a2.models to javafx.base, javafx.fxml;
 }
