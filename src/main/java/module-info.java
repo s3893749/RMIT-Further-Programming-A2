92a08bdfd16511538fb08f47818aec238b089b43
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-21 17:03:19 +1100

    - Updated module-info.java to match new file structure.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,12 +6,14 @@ module com.jackgharris.cosc2288.a2 {
     requires commons.validator;
 
     exports com.jackgharris.cosc2288.a2.core;
-    exports com.jackgharris.cosc2288.a2.controllers;
     exports com.jackgharris.cosc2288.a2.models;
     exports com.jackgharris.cosc2288.a2.controllers.components;
 
     opens com.jackgharris.cosc2288.a2.core to javafx.fxml;
-    opens com.jackgharris.cosc2288.a2.controllers to javafx.fxml;
     opens com.jackgharris.cosc2288.a2.controllers.components to javafx.fxml;
     opens com.jackgharris.cosc2288.a2.models to javafx.base, javafx.fxml;
+    exports com.jackgharris.cosc2288.a2.controllers.settings;
+    opens com.jackgharris.cosc2288.a2.controllers.settings to javafx.fxml;
+    exports com.jackgharris.cosc2288.a2.controllers.stages;
+    opens com.jackgharris.cosc2288.a2.controllers.stages to javafx.fxml;
 }
