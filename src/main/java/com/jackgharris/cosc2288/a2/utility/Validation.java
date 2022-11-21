096d538803120b032545db8a2be0fd4a5821a083
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-21 16:18:51 +1100

    - Updated isFloat check to now return false if null instead of throwing an error. - Created isInteger check to validate is a parsed string is a integer or not.

--- src/main/java/com/jackgharris/cosc2288/a2/utility/Validation.java
+++ src/main/java/com/jackgharris/cosc2288/a2/utility/Validation.java
@@ -3,6 +3,9 @@ package com.jackgharris.cosc2288.a2.utility;
 public class Validation {
 
     public static boolean isFloat(String text){
+        if(text == null){
+            return false;
+        }
 
         text = text.trim();
 
@@ -12,6 +15,26 @@ public class Validation {
 
         try{
             Float.parseFloat(text);
+        }catch (Exception e){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isInteger(String text){
+        if(text == null){
+            return false;
+        }
+
+        text = text.trim();
+
+        if(text.isBlank() || text.isEmpty()){
+            return false;
+        }
+
+        try{
+            Integer.parseInt(text);
         }catch (Exception e){
             return false;
         }
