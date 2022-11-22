039234fe55ba489e8f1f20bd47a062f28f5d5c10
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-22 12:35:59 +1100

    - Added the menuController as a class wide variable in the dashboard controller. - Added a loadLastpage method, this simply maps to the menuController setSecltionFromLastPage method.

--- src/main/java/com/jackgharris/cosc2288/a2/controllers/stages/DashboardController.java
+++ src/main/java/com/jackgharris/cosc2288/a2/controllers/stages/DashboardController.java
@@ -1,5 +1,6 @@
 package com.jackgharris.cosc2288.a2.controllers.stages;
 
+import com.jackgharris.cosc2288.a2.controllers.components.MenuController;
 import com.jackgharris.cosc2288.a2.core.MyHealth;
 import com.jackgharris.cosc2288.a2.utility.FXMLUtility;
 import javafx.fxml.FXML;
@@ -16,6 +17,9 @@ public class DashboardController{
     @FXML
     private AnchorPane menuContainerOuter;
 
+    private MenuController menuController;
+
+
 
     @FXML
     public void initialize() throws IOException {
@@ -23,17 +27,22 @@ public class DashboardController{
         FXMLLoader menuLoader = new FXMLLoader(FXMLUtility.menu);
         AnchorPane menuNodes = menuLoader.load();
         this.menuContainerOuter.getChildren().add(menuNodes.getChildren().get(0));
+        this.menuController = menuLoader.getController();
 
         FXMLLoader loader = new FXMLLoader(FXMLUtility.recentOverview);
         AnchorPane nodes = loader.load();
 
+
         AnchorPane contentOuterContainer = (AnchorPane) this.parent.getChildren().get(1);
         contentOuterContainer.getChildren().addAll(nodes.getChildren());
-
 
         //detect and apply theme changes
         this.parent.setStyle(MyHealth.getInstance().getUser().getTheme().getStyle());
         MyHealth.getInstance().setParent(this.parent);
+    }
+
+    public void loadLastPage() throws IOException {
+        this.menuController.setSelectionFromLastPage();
     }
 
 
