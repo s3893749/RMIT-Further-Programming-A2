400022a81465a90959ca94c6ab9b6e4a37bb8d05
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-11-26 10:10:18 +1100

    - Added junit dependency and exports the tests package from my module.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,12 @@ module com.jackgharris.cosc2288.a2 {
     requires org.xerial.sqlitejdbc;
     requires java.sql;
     requires commons.validator;
+    requires junit;
 
     exports com.jackgharris.cosc2288.a2.core;
     exports com.jackgharris.cosc2288.a2.models;
     exports com.jackgharris.cosc2288.a2.controllers.components;
+    exports com.jackgharris.cosc2288.a2.tests;
 
     opens com.jackgharris.cosc2288.a2.core to javafx.fxml;
     opens com.jackgharris.cosc2288.a2.controllers.components to javafx.fxml;
